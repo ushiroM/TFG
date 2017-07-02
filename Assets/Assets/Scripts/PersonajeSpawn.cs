1fcd9f17a2c07123541220433c242e28646bffd9
ushiroM <carlosgalibar@gmail.com>
2017-07-02 22:53:45 +0200

    Commit 41
    paso

--- Assets/Assets/Scripts/PersonajeSpawn.cs
+++ Assets/Assets/Scripts/PersonajeSpawn.cs
@@ -6,6 +6,7 @@ public class PersonajeSpawn : MonoBehaviour {
 
     public GameObject hombre;
     public GameObject mujer;
+    public GameObject hola;
     private bool spawn;
     private int random;
     private Transform posicion;
@@ -24,11 +25,13 @@ public class PersonajeSpawn : MonoBehaviour {
             spawn = false;
             for (int i = 0; i < 4; i++)
             {
-                random = Random.Range(1, 2);
+                random = Random.Range(1, 4);
                 if (random == 1)
                     Instantiate(hombre, posicion.position, Quaternion.identity);
-                else
+                else if(random == 2)
                     Instantiate(mujer, posicion.position, Quaternion.identity);
+                else
+                    Instantiate(hola, posicion.position, Quaternion.identity);
             }
         }
 
