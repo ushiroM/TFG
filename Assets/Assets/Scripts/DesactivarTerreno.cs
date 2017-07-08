0dcd645010a8f891828f66b5c9bfc91f48c42dc5
ushiroM <carlosgalibar@gmail.com>
2017-07-08 17:34:43 +0200

    Commit 55
    Terminada ciudad

--- Assets/Assets/Scripts/DesactivarTerreno.cs
+++ Assets/Assets/Scripts/DesactivarTerreno.cs
@@ -1,37 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
 public class DesactivarTerreno : MonoBehaviour {
     private GameObject terrain;
+    private GameObject suelo;
 
 
     void Start()
     {
         terrain = GameObject.FindGameObjectWithTag("Terrain");
+        if (EditorSceneManager.GetActiveScene().name == "Ciudad")
+            suelo = GameObject.FindGameObjectWithTag("Suelo");
     }
 
 
     public void OnTriggerEnter(Collider c)
     {
-        if (c.tag == "Camara")
+        if(EditorSceneManager.GetActiveScene().name == "escena1")
         {
-            terrain.GetComponent<Terrain>().enabled = false;
-            transform.parent.GetChild(transform.parent.childCount - 1).gameObject.SetActive(false);
-            if(transform.parent.name.Contains("Domus"))
-                transform.parent.GetChild(transform.parent.childCount - 3).gameObject.SetActive(true);
+            if (c.tag == "Camara")
+            {
+                terrain.GetComponent<Terrain>().enabled = false;
+                transform.parent.GetChild(transform.parent.childCount - 1).gameObject.SetActive(false);
+                if (transform.parent.name.Contains("Domus"))
+                    transform.parent.GetChild(transform.parent.childCount - 3).gameObject.SetActive(true);
+            }
         }
+        else
+        {
+            if (c.tag == "Camara")
+            {
+                terrain.GetComponent<Terrain>().enabled = false;
+                suelo.SetActive(false);
+                if (transform.parent.name.Contains("Domus"))
+                    transform.parent.GetChild(transform.parent.childCount - 2).gameObject.SetActive(true);
+            }
+        }
+       
 
     }
 
     public void OnTriggerExit(Collider c)
     {
-        if (c.tag == "Camara")
+        if (EditorSceneManager.GetActiveScene().name == "escena1")
         {
-            terrain.GetComponent<Terrain>().enabled = true;
-            transform.parent.GetChild(transform.parent.childCount - 1).gameObject.SetActive(true);
-            if (transform.parent.name.Contains("Domus"))
-                transform.parent.GetChild(transform.parent.childCount - 3).gameObject.SetActive(false);
+            if (c.tag == "Camara")
+            {
+                terrain.GetComponent<Terrain>().enabled = true;
+                transform.parent.GetChild(transform.parent.childCount - 1).gameObject.SetActive(true);
+                if (transform.parent.name.Contains("Domus"))
+                    transform.parent.GetChild(transform.parent.childCount - 3).gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            if (c.tag == "Camara")
+            {
+                terrain.GetComponent<Terrain>().enabled = true;
+                suelo.SetActive(true);
+                if (transform.parent.name.Contains("Domus"))
+                    transform.parent.GetChild(transform.parent.childCount - 2).gameObject.SetActive(false);
+            }
         }
     }
 }
